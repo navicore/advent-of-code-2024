a24828c67a926b39c71daff4942d6622c978ad09
Ed Sweeney <ed@onextent.com>
2024-12-01 12:31:21 -0800

    lint

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -7,13 +7,14 @@ fn part1(col1: &[i32], col2: &[i32]) {
     let paired: Vec<(&i32, &i32)> = col1.iter().zip(col2).collect();
 
     // Calculate the sum of absolute differences
+    #[allow(clippy::cast_possible_truncation)]
     let abs_diff_sum: i32 = paired
         .iter()
         .map(|(a, b)| (*a - *b).abs()) // Compute absolute difference
         .sum(); // Sum all the differences
 
     // Print the result
-    println!("Sum of absolute differences: {}", abs_diff_sum);
+    println!("Sum of absolute differences: {abs_diff_sum}");
 }
 
 fn part2(col1: &[i32], col2: &[i32]) {
@@ -24,13 +25,14 @@ fn part2(col1: &[i32], col2: &[i32]) {
     });
 
     // Compute the weighted sum
+    #[allow(clippy::cast_possible_wrap, clippy::cast_possible_truncation)]
     let weighted_sum: i32 = col1
         .iter()
         .map(|&val| val * (*col2_counts.get(&val).unwrap_or(&0) as i32))
         .sum();
 
     // Print the result
-    println!("Weighted sum: {}", weighted_sum);
+    println!("Weighted sum: {weighted_sum}");
 }
 
 pub fn run() {
