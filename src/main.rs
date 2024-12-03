bd0ca01d19a620f114a5fe5bdd0e6b68ecce79aa
Ed Sweeney <ed@onextent.com>
2024-12-03 05:19:58 -0800

    part 2 answer is  93729253

--- src/main.rs
+++ src/main.rs
@@ -16,10 +16,11 @@ enum Commands {
     Day3,
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     match Cli::parse().command {
         Commands::Day1 => days::day01::run(),
         Commands::Day2 => days::day02::run(),
-        Commands::Day3 => days::day03::run(),
+        Commands::Day3 => days::day03::run()?,
     }
+    Ok(())
 }
